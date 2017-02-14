99020fcfef94a13b5a204872fd4e13e2d05e2bd1
bergersj <joshbergers35@gmail.com>
2017-02-14 15:15:15 -0500

    styling to seating

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+$(document).ready(function(){
+
+//effect when mouse is over a particular seat
+  $('.seat').hover(function(){
+     $(this).fadeTo("fast", 0.65);
+   });
+   $('.seat').mouseleave(function(){
+      $(this).fadeTo("fast", 1.00);
+   });
+
+//effect once seat has been clicked on
+$('.seat').click(function(){
+   $(this).css('background-color', 'rgb(127, 0, 0)');
+ });
+
+});
