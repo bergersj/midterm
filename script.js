571d686b06cb72ec5a7187f2855cccd2d55ef89f
RedEis91 <imitationkrabs91@gmail.com>
2017-02-15 12:24:11 -0500

    let my module

--- script.js
+++ script.js
@@ -17,11 +17,39 @@ $('.seat').click(function(){
 });
 
 
+
+
+
+
 var reservedSeats = [];
+var reservedSeat;
+
+//created an Indivudual object
 class Individual {
   constructor(firstName, lastName, phoneNumber, emailAddress, additionalInfo){
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.phoneNumber = firstName;
-      this.emailAddress = emailAddress;
-      this.additionalInfo = additionalInfo; }
+    // set the species property of our animal to whatever parameter we pass through it
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
+    this.additionalInfo = additionalInfo;
+    return lastName;
+}
+ displayName(lastName) {
+    var lastName = lastName;
+document.write.getElementById("A1").lastName;
+}
+}
+
+// class Individual {
+//  constructor(firstName, lastName, phoneNumber, emailAddress, additionalInfo){
+//      this.firstName = firstName;
+//      this.lastName = lastName;
+//      this.phoneNumber = firstName;
+//      this.emailAddress = emailAddress;
+//      this.additionalInfo = additionalInfo;}
+//
+//    displayName(lastName, firstName) {
+//    console.log(lastName + ", " firstName);
+//  };
+  
