f35aa8eff5ec1ca4f74f2932e431d806085f0ba6
RedEis91 <imitationkrabs91@gmail.com>
2017-02-15 21:02:22 -0500

    added the JS functionality we so desperately crave

--- script.js
+++ script.js
@@ -1,70 +1,61 @@
 $(document).ready(function(){
 
 //effect when mouse is over a particular seat
-  $('.seat').hover(function(){
-     $(this).fadeTo("fast", 0.65);
-   });
-   $('.seat').mouseleave(function(){
-      $(this).fadeTo(50, 1.00);
-   });
+ $('.seat').hover(function(){
+    $(this).fadeTo("fast", 0.65);
+  });
+  $('.seat').mouseleave(function(){
+     $(this).fadeTo(50, 1.00);
+  });
 
 //effect once seat has been clicked on
 $('.seat').click(function(){
-   $(this).css('background-color', 'rgb(127, 0, 0)');
- });
+  $(this).css('background-color', 'rgb(127, 0, 0)');
+});
+
+//creates an empty array to store our objects
+var reservedSeats = [];
+var seatId;
+
+$(".seat").on("click", function(event){
+seatId = event.target.id;
+});
+
+$(".submitButton").on("click",function(event){
+ var firstName = $("#firstName").val();
+ var lastName = $("#lastName").val();
+ var phoneNumber = $("#phoneNumber").val();
+ var eMail = $("#eMail").val();
+ var extraInfo = $("#extraInfo").val();
+
+
+seatTaken = {
+ firstName: firstName,
+ lastName: lastName,
+ phoneNumber: phoneNumber,
+ email: eMail,
+ extraInfo: extraInfo,
+ seatId: seatId
+};
+reservedSeats.push(seatTaken);
+clearForm();
 
 
 });
-//creates an empty array to store our objects
-var reservedSeats = [];
-var reservedSeat;
 
+$(".seat").on("mouseenter", function(event){
+ reservedSeats.forEach(function(i) {
+   if(i.seatId === event.target.id) {
+     $(event.target).text(i.lastName);
+   }
+ })
+}).on("mouseleave", function(event) {
+ $(event.target).text(event.target.id);
+});
 
-// class Individual {
-//   constructor(formFirstName, formLastName, formPhoneNumber, formEMail, formExtraInfo){
-//     // set the species property of our animal to whatever parameter we pass through it
-//     this.firstName = formFirstName;
-//     this.lastName = formLastName;
-//     this.phoneNumber = formPhoneNumber;
-//     this.emailAddress = formEMail;
-//     this.additionalInfo = formExtraInfo;
-//   }
-// }
-
-//creates a class of Individual to create our object from
-class Individual {
-  constructor(formFirstName, formLastName, formPhoneNumber, formEMail, formExtraInfo){
-    // set the species property of our animal to whatever parameter we pass through it
-    this.firstName = formFirstName;
-    this.lastName = formLastName;
-    this.phoneNumber = formPhoneNumber;
-    this.emailAddress = formEMail;
-    this.additionalInfo = formExtraInfo;
-  }
-}
-
-//created a function to retrieve form data that is submitted by the onclick event. it places each individual attribute to a variable
-function registerSeat(){
-  var firstName = $('#firstName').val();
-  var lastName = $("#lastName").val();
-  var phoneNumber = $('#phoneNumber').val();
-  var eMail = $('#eMail').val();
-  var extraInfo = $('#extraInfo').val();
-
-    seatTaken = {};
-    seatTaken ["this.firstName"] = firstName;
-    seatTaken ["this.lastName"] = lastName;
-    seatTaken["this.phoneNumber"] = phoneNumber;
-    seatTaken ["this.eMail"] = eMail;
-    seatTaken["this.extraInfo"] = extraInfo;
-    //pushes our newly created seatTaken object into our empty reservedSeats array
-    reservedSeats.push(seatTaken);
-
-    console.log(firstName + " " + lastName);
+function clearForm(){
+ document.getElementById("userForm").reset();
 }
 
 
-//clears form after user submits
-function clearForm() {
-     document.getElementById("userForm").reset();
-}
+});
